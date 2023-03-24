2df627b4a0877afc1fd57b830b513f32e69c7ec5
nickolashkraus <0x@nickolaskraus.io>
2023-03-24 14:24:17 -0500

    Update copyright

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Nickolas Kraus. All rights reserved.
+// Copyright 2023 Nickolas Kraus. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 package main
